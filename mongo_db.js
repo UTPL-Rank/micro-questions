10634dde721801196a786f32b4d34143d115703f
lenoryv <luisortiz141999@gmail.com>
2022-08-28 18:54:18 -0500

    Add Mongo connection

---
+++ mongo_db.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+//require("dotenv").config({ path: ".env" });
+require("dotenv").config();
+
+// const { MONGO_DB_URI } = process.env;
+
+const connectionString = process.env.MONGO_DB_URI;
+
+// comment for validate .env
+if (!connectionString) {
+  console.error("Se requiere variables de entorno: MONGO_DB_URI");
+}
+
+// conexión a mongodb
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
+
+process.on("uncaughtException", (error) => {
+  console.error(error);
+  mongoose.disconnect();
+});
